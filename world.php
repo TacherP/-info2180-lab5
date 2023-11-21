357f812d59a48651d8fc041697369255f9969108
TacherP <montacher.pierre@mymona.uwi.edu>
2023-11-20 23:59:16 -0500

    used MySQL in PHP

--- world.php
+++ world.php
@@ -1,13 +1,20 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $stmt = $conn->query("SELECT * FROM countries");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+if (isset($_GET['country'])) {
+  $country = $_GET['country'];
+
+  $country = filter_var($country, FILTER_SANITIZE_STRING);
+
+}
 
 ?>
 <ul>
